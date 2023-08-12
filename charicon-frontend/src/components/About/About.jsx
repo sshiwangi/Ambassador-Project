f1cfb465781f4fb8f6294e72b7b2988cec904acb
Lilian Wanjiku <lilintech4@gmail.com>
2023-08-12 19:34:51 +0300

    Modify login page

--- charicon-frontend/src/components/About/About.jsx
+++ charicon-frontend/src/components/About/About.jsx
@@ -8,8 +8,9 @@ function About() {
         <div className="container about-section">
            <div className="section-heading">About Us</div>
            <div className="about-section-container">
-
+           <a href="../../pages/Home/Home" className="logo">
                 <img className='about-img' src={aboutImg} alt="aboutImg"  />
+                </a>
                <div className="about-text">
                We ensure transparent communication and accountability, creating a compassionate community where small acts of kindness make a significant impact. Join us in sharing hope and changing lives together!
                </div>
--- charicon-frontend/src/pages/NGOLogin/NgoLogin.jsx
+++ charicon-frontend/src/pages/NGOLogin/NgoLogin.jsx
@@ -3,7 +3,65 @@ import './ngologin.css'
 
 function NgoLogin() {
   return (
-    <div>NgoLogin</div>
+
+    <section className="outer">
+        <form>
+        <div className="container-login">
+          <div id ="section1">
+                    <div className="form-row">
+                        <label htmlFor="input1">NGO Name</label>
+                        <input type="text" id="input1" border="black"/>
+                    </div>
+                    <div className="description">
+                        <label htmlFor="input2">Description</label>
+                        <input type="text" id="input2" placeholder="Tell us something brief about the NGO" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">Country</label>
+                        <input type="text" id="input2" placeholder="Which country is the NGO situated?" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">City</label>
+                        <input type="text" id="input2" placeholder="Which city is the NGO situated?" />
+                    </div>
+                     <div className="form-row">
+                        <label htmlFor="input2">Password</label>
+                        <input type="password" id="input2" />
+                    </div>
+                    <div className="submit">
+                        <button type="submit">Next</button>
+                    </div>
+                    </div>
+
+                    <div id ="section2">
+                    <div className="form-row">
+                        <label htmlFor="input2">Username</label>
+                        <input type="text" id="input2" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">Email</label>
+                        <input type="text" id="input2" />
+                    </div>
+                     <div className="form-row">
+                        <label htmlFor="input2">State</label>
+                        <input type="text" id="input2" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">Pin Code</label>
+                        <input type="text" id="input2" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">NGO Logo</label>
+                        <input type="text" id="input2" />
+                    </div>
+                    <div className="form-row">
+                        <label htmlFor="input2">Website Link</label>
+                        <input type="text" id="input2" />
+                    </div>
+                    </div>
+                    </div>
+                </form>
+          </section>
   )
 }
 
