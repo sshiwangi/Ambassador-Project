0ac64261157fa2fb53be261c8c10ad5a32e347d7
Lilian Wanjiku <lilintech4@gmail.com>
2023-08-12 22:02:01 +0300

    Form modification

--- charicon-frontend/src/components/Navbar/Navbar.jsx
+++ charicon-frontend/src/components/Navbar/Navbar.jsx
@@ -7,14 +7,18 @@ function Navbar() {
   return (
     <section className='navbar'>
         <div className="container navbar-section ">
+        <a href="C:\Users\lili\Downloads\Charicon\charicon-frontend\src\pages\Home\Home.jsx">
             <img height="60px "src={chariconLogo} alt="charicon Logo" />
+            </a>
             <div className="nav-item-container">
               <a style={{color: 'white'}} href="#about">  <div>About</div> </a>
-              <a style={{color: 'white'}} href="#working"> <div >How it Works?</div></a> 
+              <a style={{color: 'white'}} href="#working"> <div >How it Works?</div></a>
               <a style={{color: 'white'}} href="#contact">  <div>Contact</div></a>
+              <a href="..\src\pages\UserLogin">
                 <div className="user-authentication">
-                    <img src={userIcon} alt="" />
+                    <img src={userIcon}  alt="" />
                 </div>
+                </a>
             </div>
         </div>
     </section>
--- charicon-frontend/src/pages/NGOLogin/NgoLogin.jsx
+++ charicon-frontend/src/pages/NGOLogin/NgoLogin.jsx
@@ -1,68 +1,84 @@
-import React from 'react'
-import './ngologin.css'
+import React from "react";
+import "./ngologin.css";
 
 function NgoLogin() {
   return (
-
     <section className="outer">
-        <form>
+      <form>
         <div className="container-login">
-          <div id ="section1">
-                    <div className="form-row">
-                        <label htmlFor="input1">NGO Name</label>
-                        <input type="text" id="input1" border="black"/>
-                    </div>
-                    <div className="description">
-                        <label htmlFor="input2">Description</label>
-                        <input type="text" id="input2" placeholder="Tell us something brief about the NGO" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">Country</label>
-                        <input type="text" id="input2" placeholder="Which country is the NGO situated?" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">City</label>
-                        <input type="text" id="input2" placeholder="Which city is the NGO situated?" />
-                    </div>
-                     <div className="form-row">
-                        <label htmlFor="input2">Password</label>
-                        <input type="password" id="input2" />
-                    </div>
-                    <div className="submit">
-                        <button type="submit">Next</button>
-                    </div>
-                    </div>
+          <div id="section1">
+            <div className="text">
+              <h2>Fill your details</h2>
+            </div>
+            <div className="form-row">
+              <label htmlFor="ngoname" className="required">NGO Name</label>
+              <input type="text" id="input1" border="black" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="username">Username</label>
+              <input type="text" required id="username" />
+            </div>
+            <div className="description">
+              <label htmlFor="description" className="required">Description</label>
+              <input
+                type="text"
+                id="input2"
+                placeholder="Tell us something brief about the NGO (Max 200 characters)"
+                maxLength={200}
+        required
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="country" className="required">Country</label>
+              <input
+                type="text"
+                id="input2"
+                placeholder="Which country is the NGO situated?"
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="city" className="required">City</label>
+              <input
+                type="text"
+                id="input2"
+                placeholder="Which city is the NGO situated?"
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="required" className="required">Password</label>
+              <input type="password" id="input2" />
+            </div>
+            <div className="submit">
+              <button type="submit">Next</button>
+            </div>
+          </div>
 
-                    <div id ="section2">
-                    <div className="form-row">
-                        <label htmlFor="input2">Username</label>
-                        <input type="text" id="input2" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">Email</label>
-                        <input type="text" id="input2" />
-                    </div>
-                     <div className="form-row">
-                        <label htmlFor="input2">State</label>
-                        <input type="text" id="input2" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">Pin Code</label>
-                        <input type="text" id="input2" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">NGO Logo</label>
-                        <input type="text" id="input2" />
-                    </div>
-                    <div className="form-row">
-                        <label htmlFor="input2">Website Link</label>
-                        <input type="text" id="input2" />
-                    </div>
-                    </div>
-                    </div>
-                </form>
-          </section>
-  )
+          <div id="section2">
+            <div className="email">
+              <label htmlFor="required">Email</label>
+              <input type="text" id="email" />
+            </div>
+            <div className="state">
+              <label htmlFor="required">State</label>
+              <input type="text" id="state" />
+            </div>
+            <div className="pincode">
+              <label htmlFor="required">Pin Code</label>
+              <input type="text" id="input2" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="required">NGO Logo</label>
+              <input type="text" id="input2" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="input2">Website Link</label>
+              <input type="text" id="input2" />
+            </div>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default NgoLogin
+export default NgoLogin;
